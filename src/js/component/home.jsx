998789681a72be03014022dc2243323d33042e0d
cpgravina <74896127+cpgravina@users.noreply.github.com>
2024-07-07 17:05:22 +0000

    improving responsiveness

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,10 +11,17 @@ const Home = () => {
     return () => clearInterval(interval);
   }, []);
   return (
-    <div className="container-sm">
-       <div className="d-flex justify-content-between align-items-center m-3 p-3 bg-dark text-white" style={{ height: "17rem" }}>
-        <i className="fa-solid fa-clock"></i>
-        <p className="p-2">{seconds}</p>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-6 col-md-8 col-sm-10">
+          <div
+            className="d-flex justify-content-between align-items-center m-3 p-3 bg-dark text-white"
+            style={{ height: "17rem" }}
+          >
+            <i className="icon fa-solid fa-clock"></i>
+            <p className="p-2">{seconds}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
